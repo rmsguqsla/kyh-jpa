d94c224b1934925f3d356f5f41cc62a1725f57de
Oh_geunhyeop <rmsguqsla@gmail.com>
2023-10-05 18:22:22 +0900

    주문 검색 기능 개발

--- src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -1,8 +1,15 @@
 package jpabook.jpashop.repository;
 
+import com.querydsl.core.types.Predicate;
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderStatus;
+import jpabook.jpashop.domain.QMember;
+import jpabook.jpashop.domain.QOrder;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.StringUtils;
 
 import javax.persistence.EntityManager;
 import java.util.List;
@@ -12,6 +19,10 @@ import java.util.List;
 public class OrderRepository {
 
     private final EntityManager em;
+    private final JPAQueryFactory query;
+
+    private QOrder order = QOrder.order;
+    private QMember member = QMember.member;
 
     public void save(Order order) {
         em.persist(order);
@@ -21,5 +32,28 @@ public class OrderRepository {
         return em.find(Order.class, id);
     }
 
-//    public List<Order> findAll(OrderSearch orderSearch) {}
+    public List<Order> findAll(OrderSearch orderSearch) {
+
+
+        return query
+                .select(order)
+                .from(order)
+                .join(order.member, member)
+                .where(
+                        statusEq(orderSearch.getOrderStatus()),
+                        nameLike(orderSearch.getMemberName())
+                )
+                .limit(1000)
+                .fetch();
+    }
+
+    private BooleanExpression nameLike(String memberName) {
+        return !StringUtils.hasText(memberName) ? null : member.name.contains(memberName);
+    }
+
+    private BooleanExpression statusEq(OrderStatus orderStatus) {
+        return orderStatus == null ? null : order.status.eq(orderStatus);
+    }
+
+
 }
