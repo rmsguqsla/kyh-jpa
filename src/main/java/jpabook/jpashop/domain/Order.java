72d8f19a3c41db901ea3ebaa02d9cad1185e62f4
Oh Keun-Hyeop <rmsguqsla@gmail.com>
2023-10-27 19:15:33 +0900

    주문 조회 V3.1: 엔티티를 DTO로 변환 - 페이징과 한계 돌파

--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -26,7 +26,7 @@ public class Order {
     @JoinColumn(name = "member_id")
     private Member member;
 
-    @OneToMany(mappedBy = "order", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
     private List<OrderItem> orderItems = new ArrayList<>();
 
     @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
